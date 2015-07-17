189539d5b5b2a4d40901d545363c5bcd79c3e330
Mohd Sanad <mohdsanadzakirizvi@gmail.com>
2015-07-17 10:04:50 +0530

    commit3

--- power.py
+++ power.py
@@ -1,9 +1,10 @@
 from time import sleep
 def status():
-    sleep(10)
-    power_file=open("/sys/class/power_supply/BAT0/capacity","r")
-    print power_file.readline()
+    battery_percent=open("/sys/class/power_supply/BAT0/capacity","r")
+    charging_status=open("/sys/class/power_supply/BAT0/status","r")
+    print "BATTERY :%s%%"%(battery_percent.read()),"\nSTATUS : %s"%(charging_status.readline())
+    sleep(10)   
     status()
-val = raw_input("Do you want to start? enter 1 for yes 0 for no")
+val = raw_input("Do you want to start? enter 1 for yes 0 for no\n")
 if val:
     status()
