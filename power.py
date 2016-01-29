bc01ea4d1cc2f3d6463e955254958c02db1ac327
sanad <mohdsanadzakirizvi@gmail.com>
2016-01-29 20:58:46 +0530

    applied PEP8

--- power.py
+++ power.py
@@ -1,9 +1,11 @@
 from time import sleep
+
+
 def status():
-    battery_percent=open("/sys/class/power_supply/BAT0/capacity","r")
-    charging_status=open("/sys/class/power_supply/BAT0/status","r")
-    print "BATTERY :%s%%"%(battery_percent.read()),"\nSTATUS : %s"%(charging_status.readline())
-    sleep(10)   
+    battery_percent = open("/sys/class/power_supply/BAT0/capacity", "r")
+    charging_status = open("/sys/class/power_supply/BAT0/status", "r")
+    print "BATTERY :%s%%" % (battery_percent.read()), "\nSTATUS : %s" % (charging_status.readline())
+    sleep(10)
     status()
 val = raw_input("Do you want to start? enter 1 for yes 0 for no\n")
 if val:
